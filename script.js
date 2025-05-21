10848a1f6ef4be85f7798ce11cc52c660000d38d
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-05-20 21:24:06 -0300

    feat: adiciona menu colapsável, seção de competências, currículo e responsividade para dispositivios móveis

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener('wheel', function(event) {
   }
   event.preventDefault(); // Impedir scroll padrão
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', (e) => {
+  e.stopPropagation(); // impede que o clique no botão feche o menu
+  menu.classList.toggle('active');
+});
+
+// Fecha o menu ao clicar fora
+document.addEventListener('click', (e) => {
+  // Se o menu estiver aberto e o clique for fora do menu e do botão
+  if (
+    menu.classList.contains('active') &&
+    !menu.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ) {
+    menu.classList.remove('active');
+  }
+});
